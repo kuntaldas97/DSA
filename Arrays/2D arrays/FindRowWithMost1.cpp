cd959581a92e44efe6236f472df322c7773a1b17
Kuntal Das <122853910+kuntaldas97@users.noreply.github.com>
2025-08-22 11:14:53 +0000

    Ninth commit

--- Arrays/2D arrays/FindRowWithMost1.cpp
+++ Arrays/2D arrays/FindRowWithMost1.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
+
+int findRow(vector<vector<int>> &arr, int row, int column){
+    int previousOneCount=0;
+    int currentOneCount=0;
+    int ans;
+
+    for (int i=0; i<row; i++){
+        previousOneCount=currentOneCount;
+        currentOneCount=0;
+        for(int j=0; j<column; j++){
+            if(arr[i][j]==1){
+                currentOneCount+=1;
+            }
+            if (currentOneCount>=previousOneCount){
+                ans=i;
+            }
+            else{
+                ans=i-1;
+            }
+        }
+    }
+    return ans+1;
+}
 
 int main(){
 
@@ -27,7 +51,9 @@ int main(){
         cout << endl; 
     }
 
+    int ans = findRow(arr, row, column);
 
+    cout << endl <<"The row with the highest count of 1 is : " <<  ans << endl;
 
     return 0;
 }
